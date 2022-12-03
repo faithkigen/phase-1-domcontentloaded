152b01ec0980905035d83d7ecde26000033f92b6
Faith <kigenfaith20@gmail.com>
2022-12-03 12:35:32 +0300

    completed DOM assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("The DOM has loaded");
+    let p1 = document.getElementById("text");
+    p1.textContent = "This is really cool!"
+  });
+
+
+//   let text = document.getElementById("text").textContent;
+//   document.getElementById("text").textContent = 'This is really cool!';
+//  console.log (document.getElementById("text"));
